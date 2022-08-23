b56b23ce53293c51298a0ecdf553df133664731d
FABRICIO LOPES <92175971+XFBC@users.noreply.github.com>
2022-08-22 22:48:06 -0300

    :pencil2: Pagina ( projeto form )

--- src/components/form/ProjectForm.jsx
+++ src/components/form/ProjectForm.jsx
@@ -1,14 +1,51 @@
-export default function ProjectForm() {
+import { useEffect } from 'react'
+import { useState } from 'react'
+import Input from './Input'
+import Select from './Select'
+import SubmitButton from './SubmitButton'
+
+export default function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:5000/categories', {
+      method: 'GET',
+      Headers: {
+        'Content-Type:': 'application/json'
+      }
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        setCategories(data)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+
   return (
     <div>
       <form className="flex flex-col gap-2 max-w-[30%] pt-4" action="">
-        <input type="text" placeholder="insira o nome do projeto" />
-        <input type="number" placeholder="insira o orçamento" />
-        <select name="category_id" id="">
-          <option value=""> selecione a categoria</option>
-        </select>
+        <Input
+          placeholder="insira o nome do projeto"
+          type="text"
+          name="name"
+          text="Nome do projeto"
+        />
 
-        <button>Enviar projeto</button>
+        <Input
+          type="number"
+          placeholder="orçamento do projeto"
+          name="budget"
+          text="Orçamento:"
+        />
+
+        <Select
+          name="categogy_id"
+          text="seleciona a categoria"
+          options={categories}
+        />
+
+        <SubmitButton text={btnText} />
       </form>
     </div>
   )
--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -5,7 +5,9 @@ export default function NewProject() {
     <div>
       <h1 className="text-3xl"> Criar projeto</h1>
       <p className="pt-2"> Crie seu projeto depois adicione os serviços</p>
-      <ProjectForm />
+      <ProjectForm btnText="Criar projeto" />
     </div>
   )
 }
+
+
